6e429fc640005aad938bac7a15c715fd26517437
Michael Estes <mjestes1994@gmail.com>
2016-03-15 23:00:18 -0400

    Two links now animate, hopefully properly, will find out when I animate the final link

--- model.py
+++ model.py
@@ -3,6 +3,11 @@ from sympy.printing.theanocode import theano_function
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.patches as patches
+import matplotlib as mpl
+
+mpl.rcParams['figure.dpi'] = 80
+mpl.rcParams['savefig.dpi'] = 80
 
 # DH table of this planar robot given by
 # theta_i | d_i | alpha_i | a_i
@@ -18,8 +23,6 @@ t = symbols('t')
 
 # define the q vector from the DH table
 q = Matrix([[theta1, d2, theta3]]).T
-print('\nThe q vector describing free parameters is given by:')
-pprint(q)
 
 # define standard A matrix
 A = Matrix([[cos(theta), -sin(theta), 0, 0],
@@ -36,75 +39,128 @@ A1 = A.subs([(theta, theta1), (d, 0), (alpha, pi / 2), (a, 1)])
 A2 = A.subs([(theta, 0), (d, d2), (alpha, -pi / 2), (a, 0)])
 A3 = A.subs([(theta, theta3), (d, 0), (alpha, 0), (a, 1)])
 
-# find final frame WRT base
+# find A matrices for second and final frames
+A20 = A1 * A2
 A30 = A1 * A2 * A3
 
 # Hence, p30 is rows one and two of the final column
 # as this is a planar robot with no movement in z-direction
+p10 = A1[:2, 3]
+p20 = A20[:2, 3]
 p30 = A30[:2, 3]
-print('\nThe position vector WRT base is given by:')
-pprint(p30)
 
 # compute the jacobian
 J = simplify(p30.jacobian(q))
-print('\nHence, the Jacobian matrix is given by:')
-pprint(J)
 
 # print the parametric equations describing the circle
-print('\nThe parametric equations describing the desired circle are given by:')
-print('x(t)=0.6cos(0.5t)+1.2')
-print('y(t)=0.6sin(0.5t)+1.0')
 p = Matrix([[0.6*cos(0.5*t)+1.2, 0.6*sin(0.5*t)+1.0]]).T
 
 # print the derivatives of the parametric equations
 v = diff(p, t)
-print('\nHence, the velocity vector is given by:')
-pprint(v)
 
+# compute the pseudo inverse of the symbolic jacobian matrix
+print('Computing the pseudo-inverse Jacobian, this may take some time...')
 J_inv = J.T * (J * J.T) ** -1
 q_dot = J_inv * v
 
 q_start = Matrix([[pi / 2, 0.8, -pi / 2]]).T
 dt = 4 * np.pi / 999
 
-#position = np.array(p30.subs([(theta1, q_start[0]), (d2, q_start[1]), (theta3, q_start[2])]))
-
 times = np.linspace(0, 4 * np.pi, 1000)
-position = np.zeros((2, len(times)))
-position[:, 0] = np.array(p30.subs([(theta1, q_start[0]), (d2, q_start[1]), (theta3, q_start[2])]))[:,0]
+position3 = np.zeros((2, len(times)))
+position1 = np.zeros((2, len(times)))
+position2 = np.zeros((2, len(times)))
+
+q_vals = np.zeros((3, len(times)))
+position3[:, 0] = np.array(p30.subs([(theta1, q_start[0]), (d2, q_start[1]), (theta3, q_start[2])]))[:,0]
 
 fig, ax = plt.subplots()
-line, = ax.plot(position[0, :], position[1, :])
-ax.set_xlim([0, 2])
-ax.set_ylim([0, 2])
+line, = ax.plot(position3[0, :], position3[1, :])
+arm = plt.Polygon([[0, 0], [0, 0], [0, 0]])
+floor = plt.Polygon([[-1.2, -0.2], [2, -0.2], [2, -1], [-1.2, -1]], color='lightblue')
+wall = plt.Polygon([[-1.2, -0.2], [-1.2, 2], [-1, 2], [-1, -0.2]], color='lightblue')
+ax.add_patch(arm)
+ax.add_patch(floor)
+ax.add_patch(wall)
+
+link1_pos = [[-0.1, 0], [0.1, 0], [0.1, 0.9], [0.2, 0.9], [0.2, 1.1], [-0.2, 1.1], [-0.2, 0.9], [-0.1, 0.9]]
+# add the first link of the robot
+#link1 = plt.Rectangle([-0.1, 0], 0.2, 1, 45)
+link1 = plt.Polygon([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], color='darkgoldenrod')
+#link1._transform = tr
+ax.add_patch(link1)
+
+link2 = plt.Polygon([[0, 0], [0, 0], [0, 0], [0, 0]], color='lightblue')
+ax.add_patch(link2)
+
+# add the base of the robot
+circ = plt.Circle([0, 0], 0.1, color='orange')
+ax.add_patch(circ)
+base = plt.Polygon([[-0.1, 0], [0.1, 0], [0.1,-0.2], [-0.1, -0.2]], color='orange')
+ax.add_patch(base)
+
+ax.set_xlim([-1.2, 2])
+ax.set_ylim([-1, 2])
 
 # Define expressions for computing the q_dot and p30 matrices
 dq = q_dot.subs([(theta1, theta1), (d2, d2), (theta3, theta3), (t, t)])
-p = p30.subs([(theta1, theta1), (d2, d2), (theta3, theta3)])
+p3 = p30.subs([(theta1, theta1), (d2, d2), (theta3, theta3)])
+p2 = p20.subs([(theta1, theta1), (d2, d2)])
+p1 = p10.subs([(theta1, theta1)])
 
 # Complile these expressions into C code for huge performance increase
 dq_calc = theano_function([theta1, d2, theta3, t], [dq], allow_input_downcast=True)
-p_calc = theano_function([theta1, d2, theta3], [p], allow_input_downcast=True)
+p3_calc = theano_function([theta1, d2, theta3], [p3], allow_input_downcast=True)
+p2_calc = theano_function([theta1, d2], [p2], allow_input_downcast=True)
+p1_calc = theano_function([theta1], [p1], allow_input_downcast=True)
 
 for i, time in enumerate(times):
     print(i + 1, '/1000 points calculated    \r', end='', sep='')
     dq = dq_calc(float(q_start[0]), float(q_start[1]), float(q_start[2]), time)
     q_start = q_start + dt * dq
-    position[:, i] = np.array(p_calc(float(q_start[0]),
+    q_vals[:, i] = np.array([[float(q_start[0])],
+                             [float(q_start[1])],
+                             [float(q_start[2])]])[:, 0]
+    position3[:, i] = np.array(p3_calc(float(q_start[0]),
                                      float(q_start[1]),
                                      float(q_start[2])))[:, 0]
+    position2[:, i] = np.array(p2_calc(float(q_start[0]),
+                                       float(q_start[1])))[:, 0]
+    position1[:, i] = np.array(p1_calc(float(q_start[0])))[:, 0]
 
 def animate(time):
-    global position
-    #line.set_xdata(position[0, :time])
-    #line.set_ydata(position[1, :time])
-    line.set_data(position[0, :time], position[1, :time])
-    return line,
+    global position3
+
+    # Update the line
+    line.set_data(position3[0, :time], position3[1, :time])
+    arm.set_xy([[0, 0.1], [0.1, 0], [position3[0, time], position3[1, time]]])
+
+    t_start = ax.transData
+
+    # handle second link transform
+    link2.set_xy([[position1[0, time] - 0.075, position1[1, time] + q_vals[1, time]],
+                  [position1[0, time] + 0.075, position1[1, time] + q_vals[1, time]],
+                  [position1[0, time] + 0.075, position1[1, time] + q_vals[1, time] - 1.8],
+                  [position1[0, time] - 0.075, position1[1, time] + q_vals[1, time] - 1.8]])
+    coords = t_start.transform([position1[0, time], position1[1, time]])
+    t_change = mpl.transforms.Affine2D().rotate_around(coords[0], coords[1], q_vals[0, time] - np.pi)
+    t_end = t_start + t_change
+    link2.set_transform(t_end)
+
+    # handle first link transform, initial position is locked, rotate about that position
+    coords = t_start.transform([0, 0])
+    t_change = mpl.transforms.Affine2D().rotate_around(coords[0], coords[1], q_vals[0, time] - np.pi / 2)
+    t_end = t_start + t_change
+    link1.set_xy([[-0.1, 0], [0.1, 0], [0.1, 0.9], [0.2, 0.9], [0.2, 1.1], [-0.2, 1.1], [-0.2, 0.9], [-0.1, 0.9]])
+    link1.set_transform(t_end)
+
+    # return the transformed links
+    return line, arm, link2, link1, circ
 
 def init():
     line.set_data([], [])
     return line,
 
-ani = animation.FuncAnimation(fig, animate, np.arange(position.shape[1]), interval=5, init_func=init, blit=True, repeat=False)
+ani = animation.FuncAnimation(fig, animate, np.arange(position3.shape[1]), interval=5, init_func=init, blit=True, repeat=False)
 plt.title('Trajectory of end point')
 plt.show()
