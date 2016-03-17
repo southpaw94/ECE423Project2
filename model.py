Force aspect ratio to fix animation anomalies. Pickled inverse jacobian matrix for optimization purposes.

model.py
@@ -8,9 +8,6 @@ import matplotlib.animation as animation
 import matplotlib.patches as patches
 import matplotlib as mpl
 
-mpl.rcParams['figure.dpi'] = 80
-mpl.rcParams['savefig.dpi'] = 80
-
 # DH table of this planar robot given by
 # theta_i | d_i | alpha_i | a_i
 # --------|-----|---------|----
@@ -37,11 +34,13 @@ A = Matrix([[cos(theta), -sin(theta), 0, 0],
             [0, 0, 0, 1]])
 
 # find the A matrices described by each row of the DH table
+print('Computing the individual A matrices...')
 A1 = A.subs([(theta, theta1), (d, 0), (alpha, pi / 2), (a, 1)])
 A2 = A.subs([(theta, 0), (d, d2), (alpha, -pi / 2), (a, 0)])
 A3 = A.subs([(theta, theta3), (d, 0), (alpha, 0), (a, 1)])
 
 # find A matrices for second and final frames
+print('Computing the A matrices for frames 2 and 3 WRT base...')
 A20 = A1 * A2
 A30 = A1 * A2 * A3
 
@@ -52,22 +51,33 @@ p20 = A20[:2, 3]
 p30 = A30[:2, 3]
 
 # compute the jacobian
+print('Computing the Jacobian matrix...')
 J = simplify(p30.jacobian(q))
 
 # print the parametric equations describing the circle
 p = Matrix([[0.6*cos(0.5*t)+1.2, 0.6*sin(0.5*t)+1.0]]).T
 
 # print the derivatives of the parametric equations
+print('Computing the cartesian velocity vector...')
 v = diff(p, t)
 
 # compute the pseudo inverse of the symbolic jacobian matrix
 if os.path.isfile('jacobian_inverse.pkl') == True:
+    print('Un-pickling the pseudo-inverse Jacobian file')
     J_inv = pickle.load(open('jacobian_inverse.pkl', 'rb'))
 else:
     print('Computing the pseudo-inverse Jacobian, this may take some time...')
     J_inv = J.T * (J * J.T) ** -1
 
-q_dot = J_inv * v + (Identity(3).as_explicit() - J_inv * J) * (q - q.subs([(theta1, np.pi / 2), (d2, 0.9), (theta3, np.pi / 2)])) * -1
+# compute the null solution
+print('Computing the null solution...')
+p_obs = Matrix([[1, 0]]).T
+potential = Matrix(np.zeros((3, 1)))
+potential[:2, 0] = p20 - Matrix([[1.5, 0]]).T
+potential = potential.T * potential
+eta = potential.jacobian(q)
+
+q_dot = J_inv * v + (Identity(3).as_explicit() - J_inv * J) * eta.T * -0.9
 
 q_start = Matrix([[pi / 2, 0.8, -pi / 2]]).T
 dt = 4 * np.pi / 999
@@ -81,6 +91,7 @@ q_vals = np.zeros((3, len(times)))
 position3[:, 0] = np.array(p30.subs([(theta1, q_start[0]), (d2, q_start[1]), (theta3, q_start[2])]))[:,0]
 
 fig, ax = plt.subplots()
+ax.set_aspect('equal')
 line, = ax.plot(position3[0, :], position3[1, :])
 floor = plt.Polygon([[-1.2, -0.2], [2, -0.2], [2, -1], [-1.2, -1]], color='lightblue')
 wall = plt.Polygon([[-1.2, -0.2], [-1.2, 2], [-1, 2], [-1, -0.2]], color='lightblue')
@@ -105,6 +116,8 @@ ax.add_patch(joint2)
 joint3 = plt.Circle([0, 0], 0.075, color='green')
 ax.add_patch(joint3)
 
+line1 = plt.Line2D([0, 0], [0, 0], linewidth=5)
+
 # add the final link
 link3 = plt.Polygon([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], color='purple')
 ax.add_patch(link3)
@@ -115,7 +128,7 @@ ax.add_patch(circ)
 base = plt.Polygon([[-0.1, 0], [0.1, 0], [0.1,-0.2], [-0.1, -0.2]], color='orange')
 ax.add_patch(base)
 
-ax.set_xlim([-1.2, 2])
+ax.set_xlim([-1.2, 3])
 ax.set_ylim([-1, 2])
 
 # Define expressions for computing the q_dot and p30 matrices
@@ -129,6 +142,8 @@ dq_calc = theano_function([theta1, d2, theta3, t], [dq], allow_input_downcast=Tr
 p3_calc = theano_function([theta1, d2, theta3], [p3], allow_input_downcast=True)
 p2_calc = theano_function([theta1, d2], [p2], allow_input_downcast=True)
 p1_calc = theano_function([theta1], [p1], allow_input_downcast=True)
+
+print('Computing the position at each of 1000 points...')
 
 for i, time in enumerate(times):
     print(i + 1, '/1000 points calculated    \r', end='', sep='')
@@ -160,7 +175,7 @@ def animate(time):
     link1.set_transform(t_end)
 
     # handle the movement of the second joint
-    joint2.center = [position3[0, time], position3[1, time]]
+    joint2.center = [position1[0, time], position1[1, time]]
 
     # handle second link transform
     link2.set_xy([[position1[0, time] - 0.075, position1[1, time] + q_vals[1, time]],
@@ -178,7 +193,7 @@ def animate(time):
     # handle the third link transform
     link3.set_xy([[position2[0, time] - 0.075, position2[1, time]],
                   [position2[0, time] - 0.075, position2[1, time] + 0.8],
-                  [position2[0, time], position2[1, time] + 1 - 0.075],
+                  [position2[0, time], position2[1, time] + 1],
                   [position2[0, time] + 0.075, position2[1, time] + 0.8],
                   [position2[0, time] + 0.075, position2[1, time]]])
     coords = t_start.transform([position2[0, time], position2[1, time]])
@@ -187,7 +202,7 @@ def animate(time):
     link3.set_transform(t_end)
 
     # return the transformed links
-    return line, link2, link1, circ, link3, joint3, joint2
+    return link2, link1, circ, link3, joint3, joint2, line 
 
 def init():
     line.set_data([], [])